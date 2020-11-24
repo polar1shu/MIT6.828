bb4b06043e5692e73c74652537a40fca36ba29be
root <root@10.245.150.34>
2020-11-24 16:06:15 +0800

    lab3 alloc

--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+//   struct file file[NFILE];
 } ftable;
 
 void
@@ -32,13 +32,23 @@ filealloc(void)
   struct file *f;
 
   acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
+  f = bd_malloc(sizeof(struct file));
+  if (f) {
     if(f->ref == 0){
-      f->ref = 1;
-      release(&ftable.lock);
-      return f;
+        memset(f,0,sizeof(struct file));
+        f->ref = 1;
+        release(&ftable.lock);
+        return f;
     }
   }
+
+//   for(f = ftable.file; f < ftable.file + NFILE; f++){
+//     if(f->ref == 0){
+//       f->ref = 1;
+//       release(&ftable.lock);
+//       return f;
+//     }
+//   }
   release(&ftable.lock);
   return 0;
 }
@@ -68,6 +78,8 @@ fileclose(struct file *f)
     release(&ftable.lock);
     return;
   }
+
+//   bd_print();
   ff = *f;
   f->ref = 0;
   f->type = FD_NONE;
@@ -80,6 +92,9 @@ fileclose(struct file *f)
     iput(ff.ip);
     end_op(ff.ip->dev);
   }
+  
+  bd_free(f);
+
 }
 
 // Get metadata about file f.
